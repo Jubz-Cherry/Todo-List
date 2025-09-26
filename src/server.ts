18abac5bd1f85fb448548d98ed364f9823bb8dc0
Jubz-Cherry <juliapnascimento15@gmail.com>
2025-09-26 12:52:13 -0300

    add axios and cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import { connectDB } from './config/db';
 import routes from './routes/todoRoutes';
+import cors from 'cors'; 
+import axios from 'axios';  
+
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+app.use(cors());
+app.use(axios);
+
 
 connectDB(); 
 
